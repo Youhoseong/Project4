c87bb0349121d133c7851bea3005f1764f4eea73
Youhoseong <h0song@naver.com>
2020-05-16 21:40:30 +0900

    초기 커밋.

---
+++ Project4/소스.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <String>
+#include <stdio.h>
+
+using namespace std;
+
+struct account {
+	int acntID;
+	string name;
+	int howmuchLeft;
+
+}Account;
+
+void creatAccount(int money, string name) {
+
+
+
+}
+
+int main(void) {
+	while (1) {
+		cout << "1. 계좌개설" << endl;
+		cout << "2. 입	  금" << endl;
+		cout << "3. 출	  금" << endl;
+		cout << "4. 전체고객 잔액조회" << endl;
+		cout << "5. 프로그램 종료" << endl;
+		cout << "선택: ";
+
+
+		int n;
+		cin >> n;
+
+		if (n == 1) {
+
+		}
+		else if (n == 2) {
+
+		}
+		else if (n == 3) {
+
+		}
+		else if (n == 4) {
+
+		}
+		else if (n == 5) {
+			break;
+		}
+
+	}
+
+	return 0;
+} 
+
+
